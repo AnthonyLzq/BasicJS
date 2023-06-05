c5618d3a65417087fa6406ab28808b792bddec6b
AnthonyLzq <sluzquinosa@uni.pe>
2023-06-04 22:55:19 -0500

    feat: implemented async js with promises and async-await

--- async.js
+++ async.js
@@ -19,16 +19,16 @@ function expensiveFunction(initialValue) {
 
 function main() {
   lazyFunction((sum) => {
-    console.log(`Primer resultado: ${sum}`)
+    console.log(`Primer resultado con callbacks: ${sum}`)
 
     lazyFunction((sum2) => {
-      console.log(`Segundo resultado: ${sum2}`)
+      console.log(`Segundo resultado con callbacks: ${sum2}`)
 
       lazyFunction((sum3) => {
-        console.log(`Tercer resultado: ${sum3}`)
+        console.log(`Tercer resultado con callbacks: ${sum3}`)
 
         lazyFunction((sum4) => {
-          console.log(`Cuarto resultado: ${sum4}`)
+          console.log(`Cuarto resultado con callbacks: ${sum4}`)
         }, sum3)
       }, sum2)
     }, sum)
@@ -42,4 +42,73 @@ main()
 // 3. pido un enterprise con el customer_enterprise_id
 // 4. pido los afiliados con el enterprise_affiliates
 
-// Promesas / async-await
+// Promesas
+
+function lazyFunctionWithPromise(initialValue = 0) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Me demoré 1s')
+
+      const sum = expensiveFunction(initialValue)
+
+      resolve(sum)
+    }, 1_000)
+  })
+}
+
+function mainWithPromise() {
+  lazyFunctionWithPromise()
+    .then(sum => {
+      console.log(`Primer resultado con promesas: ${sum}`)
+
+      return lazyFunctionWithPromise(sum)
+    })
+    .then(sum2 => {
+      console.log(`Segundo resultado con promesas: ${sum2}`)
+
+      return lazyFunctionWithPromise(sum2)
+    })
+    .then(sum3 => {
+      console.log(`Tercer resultado con promesas: ${sum3}`)
+
+      return lazyFunctionWithPromise(sum3)
+    })
+    .then(sum4 => {
+      console.log(`Cuarto resultado con promesas: ${sum4}`)
+      return sum4
+    })
+    // .catch(error => {
+    //   console.log('Hubo un error', error)
+    // })
+    // .finally(() => {
+    //   console.log('Este código se ejecuta al final de todo')
+    // })
+}
+
+mainWithPromise()
+
+// Async - await
+
+async function mainWithAsyncAwait() {
+  try {
+    const sum = await lazyFunctionWithPromise()
+    console.log(`Primer resultado con async-await: ${sum}`)
+
+    const sum2 = await lazyFunctionWithPromise(sum)
+    console.log(`Segundo resultado con async-await: ${sum2}`)
+
+    const sum3 = await lazyFunctionWithPromise(sum2)
+    console.log(`Tercer resultado con async-await: ${sum3}`)
+
+    const sum4 = await lazyFunctionWithPromise(sum3)
+    console.log(`Cuarto resultado con async-await: ${sum4}`)
+  } catch (error) {
+    console.log('Hubo un error', error)
+  }
+
+  const aux = await lazyFunctionWithPromise().catch(error => {
+    console.log('Hubo un error: ', error)
+  })
+}
+
+mainWithAsyncAwait()
